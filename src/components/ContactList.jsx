3c7656a9097ee27ab7f5c51bb455733ca0e4cb5a
cristinamacicasan <cmacicasan@gmail.com>
2024-04-15 07:02:09 +0300

    add contactlist

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContactList.module.css';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <div className={styles.container}>
+      <ul>
+        {contacts.map(({ id, name }) => (
+          <li className={styles.contact} key={id}>
+            {name}
+            <button type="button" onClick={() => onDeleteContact(id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
